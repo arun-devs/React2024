ab1dbe47bf05a2325357ed5b5887016febae0832
Arun <arundevarajan346@gmail.com>
2024-11-21 20:54:36 +0530

    day-3

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 // const heading=React.createElement(
 //     "h1",
 //     {id:"heading"
@@ -9,12 +11,36 @@
 
 
 //nested elements in react
-const headingnew=React.createElement("div",{id:"heading"},
-    React.createElement("div",{id:"heading2"},
-        [React.createElement("hi",{id:"heading3"},"hii"),React.createElement("hi",{id:"heading4"},"hii2")])
-    )
+// const headingnew=React.createElement("div",{id:"heading"},
+//     React.createElement("div",{id:"heading2"},
+//         [React.createElement("hi",{id:"heading3"},"hii"),React.createElement("hi",{id:"heading4"},"hii2")])
+//     )
 
-const root=ReactDOM.createRoot(document.getElementById("root"))
-const final=root.render(headingnew)
-console.log(headingnew)
+// const root=ReactDOM.createRoot(document.getElementById("root"))
+// const final=root.render(headingnew)
+// console.log(headingnew)
 
+
+//jsx
+
+// const jsxheading = <h1 id="heading">my jsx react</h1>;
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxheading);
+// console.log(jsxheading);
+
+//functional componet
+// const FunctionalComponent=()=>
+// {return <h1>hii arun</h1>;
+
+// }
+const HeadingCmp=()=><h1>welcome</h1>
+
+const FunctionalComponent=()=>(
+    <div>
+    <HeadingCmp/>
+    <h1 id="heading" className="heading">hii arundev</h1></div>
+)
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FunctionalComponent/>);
