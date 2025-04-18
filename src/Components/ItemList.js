0b0459b7f21c78bc485e718a5bb0b33a36bb02df
Arun <arundevarajan346@gmail.com>
2025-04-18 08:27:03 +0530

    tt

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -2,19 +2,25 @@ import { RES_URL } from "../utils/constant";
 
 const ItemList=({itemss})=>{
     return(
-        <div>{itemss.map((item)=>(<div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 flex">
-           <img src={RES_URL + item.card.info.imageId} className="w-14"/>
-           <div>
-           <div className="py-2">
+        <div>{itemss.map((item)=>(<div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 flex justify-between">
+         
+           
+           <div className="w-9/12">
+           <div className="py-2 ">
                 <span>{item.card.info.name}</span>
                 <span>-{item.card.info.price/100}</span>
                 
                 
          </div>
-         <div><span className="bg-cyan-400">{item.card.info.ribbon.text}</span></div>
+         <div><span className="bg-cyan-400 align-baseline p-0">{item.card.info.ribbon.text}</span></div>
            </div>
+           <div className="w-3/12 p-4">
+            <div className="absolute">
+                <button className="p-2 shadow-amber-50 bg-blue-50">Add+</button>
+            </div>
+            <img src={RES_URL + item.card.info.imageId} className="w-full"/>
+            </div>
            
-            
             
             
             
